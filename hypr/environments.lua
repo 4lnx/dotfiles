2824afa594f378324438bed2af7d407c8c5f77f7
4Lnx <4lnx.notfound@gmail.com>
2026-06-12 15:56:11 -0400

    feat: add Hyprland desktop configs (hypr, waybar, swaync, rofi)

---
+++ hypr/environments.lua
@@ -0,0 +1,22 @@
+-------------------------------
+---- ENVIRONMENT VARIABLES ----
+-------------------------------
+
+-- See https://wiki.hypr.land/Configuring/Advanced-and-Cool/Environment-variables/
+
+hl.env("XCURSOR_SIZE", "24")
+hl.env("HYPRCURSOR_SIZE", "24")
+
+hl.env("XDG_CURRENT_DESKTOP", "Hyprland")
+hl.env("XDG_SESSION_TYPE", "wayland")
+hl.env("XDG_SESSION_DESKTOP", "Hyprland")
+
+hl.env("GDK_BACKEND", "wayland,x11,*")
+hl.env("QT_QPA_PLATFORM", "wayland;xcb")
+
+hl.env("QT_QPA_PLATFORMTHEME", "qt5ct")
+hl.env("QT_WAYLAND_DISABLE_WINDOWDECORATION", "1")
+
+hl.env("XDG_MENU_PREFIX", "arch-")
+
+hl.env("SSH_AUTH_SOCK", "/run/user/1000/gnupg/S.gpg-agent.ssh")
---
+++ hypr/windows.lua
@@ -0,0 +1,71 @@
+--------------------------------
+---- WINDOWS AND WORKSPACES ----
+--------------------------------
+
+-- See https://wiki.hypr.land/Configuring/Basics/Window-Rules/
+-- and https://wiki.hypr.land/Configuring/Basics/Workspace-Rules/
+
+-- Example window rules that are useful
+
+local suppressMaximizeRule = hl.window_rule({
+	-- Ignore maximize requests from all apps. You'll probably like this.
+	name = "suppress-maximize-events",
+	match = { class = ".*" },
+
+	suppress_event = "maximize",
+})
+-- suppressMaximizeRule:set_enabled(false)
+
+-- Workspace 5 sempre no monitor HDMI-A-2
+hl.workspace_rule({
+	workspace = "10",
+	monitor = "HDMI-A-2",
+})
+
+-- hl.window_rule({
+-- 	match = {
+-- 		class = "^(brave-browser)$",
+-- 	},
+--
+-- 	workspace = "10 ",
+-- })
+--
+-- hl.window_rule({
+-- 	match = {
+-- 		class = "^(org.keepassxc.KeePassXC)$",
+-- 	},
+--
+-- 	workspace = "1",
+-- })
+--
+hl.window_rule({
+	-- Fix some dragging issues with XWayland
+	name = "fix-xwayland-drags",
+	match = {
+		class = "^$",
+		title = "^$",
+		xwayland = true,
+		float = true,
+		fullscreen = false,
+		pin = false,
+	},
+
+	no_focus = true,
+})
+
+-- Layer rules also return a handle.
+-- local overlayLayerRule = hl.layer_rule({
+--     name  = "no-anim-overlay",
+--     match = { namespace = "^my-overlay$" },
+--     no_anim = true,
+-- })
+-- overlayLayerRule:set_enabled(false)
+
+-- Hyprland-run windowrule
+hl.window_rule({
+	name = "move-hyprland-run",
+	match = { class = "hyprland-run" },
+
+	move = "20 monitor_h-120",
+	float = true,
+})
