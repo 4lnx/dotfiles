3c5bddcda9afaa69d31c8e8cd8df04df097e19fa
4Lnx <4lnx.notfound@gmail.com>
2026-07-02 19:41:57 -0400

    added xml formater

---
+++ nvim/lua/plugins/conform.lua
@@ -0,0 +1,21 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        xml = { "xmllint" },
+      },
+
+      formatters = {
+        xmllint = {
+          command = "xmllint",
+          args = {
+            "--format",
+            "-",
+          },
+          stdin = true,
+        },
+      },
+    },
+  },
+}
